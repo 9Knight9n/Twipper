706ffb703fec3049878eb6150e013f56b5f9b877
Mohamad Sajad Naghizadeh <naghizadeh.mdsd@gmail.com>
2022-07-26 10:26:23 +0430

    fixed react hot reload

---
+++ frontend/src/pages/ExtractPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// import './App.css';
+
+function ExtractPage() {
+  return (
+    <React.Fragment>
+        hello
+    </React.Fragment>
+  );
+}
+
+export default ExtractPage;
