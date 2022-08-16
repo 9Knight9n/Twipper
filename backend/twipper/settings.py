ba6bbbeb4e15df0c6fe24082b6795d3b67e91b2b
Mohamad Sajad Naghizadeh <naghizadeh.mdsd@gmail.com>
2022-08-17 01:10:36 +0430

    Update settings.py

--- backend/twipper/settings.py
+++ backend/twipper/settings.py
@@ -27,9 +27,14 @@ SECRET_KEY = 'django-insecure-+fs^q*$$^+_d8dfk0%)7^@1(2ihg8(67r00@=a=yt)3!#ka8-9
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-CORS_ALLOWED_ORIGINS = [
-    '*',
+
+CORS_ALLOWED_ORIGIN_REGEXES =[
+    r'^.+$'
 ]
+
+# CORS_ALLOWED_ORIGINS = [
+#     '*',
+# ]
 
 
 # Application definition
@@ -52,6 +57,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -59,8 +66,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'twipper.urls'
